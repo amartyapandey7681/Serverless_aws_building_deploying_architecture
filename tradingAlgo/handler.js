4272e6d083d2e21ae49812e467bb48aec696a43d
Amartya Pandey <amartya.pandey@pristyncare.com>
2022-05-09 22:46:05 +0530

    initial changes

--- tradingAlgo/handler.js
+++ tradingAlgo/handler.js
@@ -9,38 +9,10 @@ const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 
 app.use(express.json());
 
-app.get("/users/:userId", async function (req, res) {
-  const params = {
-    TableName: USERS_TABLE,
-    Key: {
-      userId: req.params.userId,
-    },
-  };
 
-  try {
-    const { Item } = await dynamoDbClient.get(params).promise();
-    if (Item) {
-      const { userId, name } = Item;
-      res.json({ userId, name });
-    } else {
-      res
-        .status(404)
-        .json({ error: 'Could not find user with provided "userId"' });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not retreive user" });
-  }
-});
-
-app.post("/users", async function (req, res) {
-  const { userId, name } = req.body;
-  if (typeof userId !== "string") {
-    res.status(400).json({ error: '"userId" must be a string' });
-  } else if (typeof name !== "string") {
-    res.status(400).json({ error: '"name" must be a string' });
-  }
-
+app.post("/mainApi", async function (req, res) {
+  const {  } = req.body;
+ 
   const params = {
     TableName: USERS_TABLE,
     Item: {
@@ -50,8 +22,9 @@ app.post("/users", async function (req, res) {
   };
 
   try {
-    await dynamoDbClient.put(params).promise();
-    res.json({ userId, name });
+    // await dynamoDbClient.put(params).promise();
+    // res.json({ userId, name });
+    //querying 
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Could not create user" });
